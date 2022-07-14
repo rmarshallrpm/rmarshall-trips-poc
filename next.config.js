7070cc21774b4a93d05de01600c9901a6b799c18
Roger Marshall <rmarshallrpm@gmail.com>
2022-07-14 15:36:51 -0700

    Missing additive filters. bug on multi select.
    Signed-off-by: Roger Marshall <rmarshallrpm@gmail.com>

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['cms.inspirato.com']
+  }, 
+  publicRuntimeConfig: {
+    cmsUrl: 'https://cms.inspirato.com'
+  } 
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
+import '../styles/globals.scss';
+import '../styles/header.scss';
+import '../styles/footer.scss';
+
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
   return <Component {...pageProps} />
 }
 
